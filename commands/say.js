b4cff50aaf6b926083a6e5e1618edf8e09d99daf
Brandon Escalona <dunkston@gmail.com>
2018-09-20 16:08:52 -0700

    Restored backup

--- commands/say.js
+++
@@ -1,9 +0,0 @@
-process.on('unhandledRejection', error => console.error(`Uncaught Promise Rejection:\n${error}`));
-
-module.exports = {
-    name: 'say',
-    execute(message, args, client, Users) {
-        message.channel.send(message.content.substring(5)).then(msg => msg.delete(30000));
-        message.delete();
-    }
-}
